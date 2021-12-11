82dbb6c28f009d7d831f7acea8609c0bcf9e399f
marvinxkiyingi <marvin96k@gmail.com>
2021-12-11 13:36:21 +0100

    Changed from class to functional component and removed default react styling

---
+++ src/components/componentA/componentA.tsx
@@ -0,0 +1,7 @@
+export const ComponentA = () => {
+  return (
+    <div className={'component'}>
+      <span className={'component_title'}> Component A </span>
+    </div>
+  );
+};
---
+++ src/components/componentB/componentB.tsx
@@ -0,0 +1,7 @@
+export const ComponentB = () => {
+  return (
+    <div className={'component'}>
+      <span className={'component_title'}> Component B </span>
+    </div>
+  );
+};
